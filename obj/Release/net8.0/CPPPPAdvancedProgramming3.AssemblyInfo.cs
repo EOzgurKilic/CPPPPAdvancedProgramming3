d4c6cbf21440c5d0b24ac81278f1b78f4e9c216d
Özgür KILIÇ <ozgurkilic147@gmail.com>
2024-12-29 21:52:19 +0100

    Removed Hello World

--- obj/Release/net8.0/CPPPPAdvancedProgramming3.AssemblyInfo.cs
+++ obj/Release/net8.0/CPPPPAdvancedProgramming3.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPPPPAdvancedProgramming3")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031a5e70e53b176bed4dddfad4cdd94b1e1f5b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPPPPAdvancedProgramming3")]
